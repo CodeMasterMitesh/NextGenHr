4d987463c26a30ba6513f27f95d93ae5be6026f6
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-11-13 09:16:02 +0530

    update

--- mongo_collection/Designation.js
+++ mongo_collection/Designation.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const designationSchema = new mongoose.Schema({
   title: { type: String, required: true },
   department_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Department' },
+  company_id : { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
+  branch_id : { type: mongoose.Schema.Types.ObjectId, ref: 'Branch' },
   description: String
 }, { timestamps: true });
 
